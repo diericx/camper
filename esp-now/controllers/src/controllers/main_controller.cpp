9ad47f25c0f80a533e6fde0f33d6e1ccbd686a6b
Zac Holland <zacharyholland@gmail.com>
2025-09-04 14:35:51 -0700

    switch roles

--- esp-now/controllers/src/controllers/main_controller.cpp
+++ esp-now/controllers/src/controllers/main_controller.cpp
@@ -3,10 +3,14 @@
 #include <esp_now.h>
 #include "messages.h"
 
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+esp_now_peer_info_t peerInfo;
+
 MainController mainController;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+void OnRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
   // Create a struct_message called myData
   Header header;
@@ -27,11 +31,50 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
   Serial.println();
 }
 
+// callback when data is sent
+void OnSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
 void MainController::init()
 {
-  esp_now_register_recv_cb(OnDataRecv);
+  // Register peer(s)
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnRecv);
+  esp_now_register_send_cb(OnSent);
 }
 
 void MainController::update()
 {
+  // Create a struct_message called myData
+  Heartbeat msg;
+
+  // Set values to send
+  strcpy(msg.msg, "THIS IS A HEARTBEAT");
+  msg.src = ControllerType::RearCamera;
+  msg.dest = ControllerType::Main;
+  msg.msgType = MessageType::Heartbeat;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&msg, sizeof(msg));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
 }
--- esp-now/controllers/src/controllers/rear_camera_controller.cpp
+++ esp-now/controllers/src/controllers/rear_camera_controller.cpp
@@ -5,54 +5,32 @@
 
 RearCameraController rearCamController;
 
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-esp_now_peer_info_t peerInfo;
-
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+// callback function that will be executed when data is received
+void OnRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  // Create a struct_message called myData
+  Header header;
+  memcpy(&header, incomingData, sizeof(header));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Source type: ");
+  Serial.println(header.src);
+  Serial.print("Dest type: ");
+  Serial.println(header.dest);
+  Serial.print("Msg type: ");
+  Serial.println(MessageTypeToString(header.msgType));
+
+  Heartbeat msg;
+  memcpy(&msg, incomingData, sizeof(msg));
+  Serial.print("Heartbeat Content: ");
+  Serial.println(msg.msg);
+  Serial.println();
 }
 
 void RearCameraController::init()
 {
-  esp_now_register_send_cb(OnDataSent);
-
-  // Register peer(s)
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
-  {
-    Serial.println("Failed to add peer");
-    return;
-  }
 }
 
 void RearCameraController::update()
 {
-
-  // Create a struct_message called myData
-  Heartbeat msg;
-
-  // Set values to send
-  strcpy(msg.msg, "THIS IS A HEARTBEAT");
-  msg.src = ControllerType::RearCamera;
-  msg.dest = ControllerType::Main;
-  msg.msgType = MessageType::Heartbeat;
-
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&msg, sizeof(msg));
-
-  if (result == ESP_OK)
-  {
-    Serial.println("Sent with success");
-  }
-  else
-  {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
 }
